c2e2035ac580572e8bdda483cda14a46598f2a3b
romomartin <romoander@gmail.com>
2024-09-22 13:28:39 +0200

    Fix vertical animation glitch

--- script.js
+++ script.js
@@ -3,6 +3,41 @@ const burgerPriceBtc = 0.00000001 * burgerPriceSat
 let eurRate = 0
 const apiUrl = "http://api.coinlayer.com/live?target=EUR&access_key=8016b6d9b9194fea53585ccef7e1449f";
 
+getStylePropPixels = (pixelString)=>{
+    return Number(pixelString.substring(0,pixelString.length - 2))
+}
+
+const bgColumn = document.getElementsByClassName("column")[0]
+const bgColumnStyle = window.getComputedStyle(bgColumn)
+const bgColumnHeight = getStylePropPixels(bgColumnStyle.height)
+
+const burgerIcon = document.getElementsByClassName("burger")[10]
+const burgerIconStyle = window.getComputedStyle(burgerIcon)
+const burgerIconHeight = 
+    getStylePropPixels(burgerIconStyle.marginTop) + 
+    getStylePropPixels(burgerIconStyle.height)
+
+const burgerAnimationEnd = Math.ceil(bgColumnHeight%burgerIconHeight)
+
+const btcIcon = document.getElementsByClassName("btc")[10]
+const btcIconStyle = window.getComputedStyle(btcIcon)
+const btcIconHeight = 
+    getStylePropPixels(btcIconStyle.marginTop) + 
+    getStylePropPixels(btcIconStyle.height)
+
+const btcAnimationStart = Math.ceil(bgColumnHeight%btcIconHeight)
+
+console.log(btcIconHeight)
+console.log(bgColumnHeight)
+console.log(burgerAnimationEnd)
+console.log(btcAnimationStart)
+
+
+
+let root = document.documentElement;
+root.style.setProperty('--burger-end-y', `-${burgerAnimationEnd}px`);
+root.style.setProperty('--btc-start-y', `-${btcAnimationStart}px`);
+
 fetch(apiUrl)
     .then(response => {
         if (!response.ok) {
